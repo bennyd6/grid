95c50492804fa06bd85f3ff0211f5a05c66404d4
bennyd6 <228r1a66d6@cmrec.ac.in>
2025-07-04 22:49:01 +0530

    typo

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,17 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-const MONGO_URI="mongodb+srv://cadheshbenny:snmXLDw0Q5XwwiID@cluster0.flih7yf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
+const MONGO_URI = process.env.MONGO_URI;
 
 const connectToMongo = () => {
-    mongoose.connect(MONGO_URI)
+    mongoose.connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
         .then(() => console.log("MongoDB Connected...."))
         .catch((e) => {
             console.error("MongoDB connection error:", e);
         });
 };
 
-
-module.exports=connectToMongo
+module.exports = connectToMongo;
